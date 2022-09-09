2cc5609d2dfdb309560f4d4e99781162b903c91d
Jingying Huang <huang.jingyin@northeastern.edu>
2022-09-09 11:41:24 -0400

    first commit

---
+++ Connect/LoginView.swift
@@ -0,0 +1,8 @@
+//
+//  LoginView.swift
+//  Connect
+//
+//  Created by Jingying(Frieda) Huang on 2022/9/9.
+//
+
+import Foundation
---
+++ Connect/SignUpView.swift
@@ -0,0 +1,8 @@
+//
+//  SignUpView.swift
+//  Connect
+//
+//  Created by Jingying(Frieda) Huang on 2022/9/9.
+//
+
+import Foundation

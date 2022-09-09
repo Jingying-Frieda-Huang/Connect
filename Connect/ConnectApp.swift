bb4454bdec25f5c44e3bd9c9ce51604410a80ac1
Jingying Huang <wphuang@ucdavis.edu>
2022-09-09 08:08:35 -0400

    Initial Commit

---
+++ Connect/ConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConnectApp.swift
+//  Connect
+//
+//  Created by Jingying(Frieda) Huang on 2022/9/9.
+//
+
+import SwiftUI
+
+@main
+struct ConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
